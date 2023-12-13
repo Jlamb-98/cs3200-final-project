ae2ad42b0c38fa46e031cc8f1d5dc41a4aa28680
Joseph Lamb <jdlamb16@gmail.com>
2023-12-12 18:13:22 -0700

    User model has list of trainingPlanId and Workout has hardcoded list of usernames

--- TrainingPlanner/app/src/main/java/com/example/trainingplanner/ui/models/User.kt
+++ TrainingPlanner/app/src/main/java/com/example/trainingplanner/ui/models/User.kt
@@ -3,7 +3,7 @@ package com.example.trainingplanner.ui.models
 data class User(
     val id: String? = null,
     val username: String? = null,
-//    val trainingPlanId: String? = null, // could be a list of id's
+    val trainingPlanId: List<String?> = listOf(null), // could be a list of id's
 //    val role: String? = null,
     // photo, first name
 )
--- TrainingPlanner/app/src/main/java/com/example/trainingplanner/ui/models/Workout.kt
+++ TrainingPlanner/app/src/main/java/com/example/trainingplanner/ui/models/Workout.kt
@@ -6,14 +6,15 @@ data class Workout(
     val id: String? = null,
     val title: String? = null,
     val description: String? = null,
-    val day: Int? = null,
-    val month: Int? = null,
-    val year: Int? = null,
+    val date: String? = null,
     val restDay: Boolean? = null,
-    val membersCompleted: List<String?> = mutableListOf(null)   // list of member usernames
+    val membersCompleted: List<String?> = mutableListOf(null)   // list of usernames, not really dynamic
+//    val day: Int? = null,
+//    val month: Int? = null,
+//    val year: Int? = null,
 //    val userCompleted: Boolean? = null,
 ) {
-    fun getDate(): String {
-        return LocalDate.of(year!!, month!!, day!!).toString()
-    }
+//    fun getDate(): String {
+//        return LocalDate.of(year!!, month!!, day!!).toString()
+//    }
 }
